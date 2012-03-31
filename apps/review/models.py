ae980c8a4eed72e57a17885f7f4f8450661e9c63
Dennis Ai <dennis.q.ai@gmail.com>
2012-03-30 22:24:53 -0500

    Setup review models

--- apps/review/models.py
+++ apps/review/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
+from item.models import Item
 
-# Create your models here.
+class Review(models.Model):
+    RATING_CHOICES = [(i,i) for i in range(1,6)] 
+
+    user = models.ForeignKey(User)
+    item = models.ForeignKey(Item)
+    text = models.TextField()
+    rating = models.SmallIntegerField(choices=RATING_CHOICES)
+     
+    def __unicode__(self):
+        return "%s reviewing Item #%s" % (self.user.get_full_name(),
+            self.item.id)
+
+class Agree(models.Model):
+    giver = models.ForeignKey(User)
+    review = models.ForeignKey(Review)
+    created_at = DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return "%s agreeing with Review #:%s" % \
+            (self.giver.get_full_name(), self.review.id)
+
+class Thank(models.Model):
+    giver = models.ForeignKey(User)
+    review = models.ForeignKey(Review)
+    created_at = DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return "%s thanking Review #:%s" \
+            (self.giver.get_full_name(), self.review.id)
