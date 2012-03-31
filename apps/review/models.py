ac59aeb6fe804d1345b6dd9c998a5d6b1e5e11c6
Dennis Ai <dennis.q.ai@gmail.com>
2012-03-30 23:37:16 -0500

    Added signal for creating a review

--- apps/review/models.py
+++ apps/review/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.db.models import F
+
 from item.models import Item
+from user_profile.models import UserStatistics
 
 class Review(models.Model):
     RATING_CHOICES = [(i,i) for i in range(1,6)] 
@@ -17,7 +22,7 @@ class Review(models.Model):
 class Agree(models.Model):
     giver = models.ForeignKey(User)
     review = models.ForeignKey(Review)
-    created_at = DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __unicode__(self):
         return "%s agreeing with Review #:%s" % \
@@ -26,8 +31,22 @@ class Agree(models.Model):
 class Thank(models.Model):
     giver = models.ForeignKey(User)
     review = models.ForeignKey(Review)
-    created_at = DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __unicode__(self):
         return "%s thanking Review #:%s" \
             (self.giver.get_full_name(), self.review.id)
+
+@receiver(post_save, sender=Review)
+def review_update(sender, instance, created, **kwargs):
+    if created:
+        # Increment the user's review count by one and karma by five
+        UserStatistics.objects.filter(pk__exact=instance.user_id).update(
+            reviews=F('reviews')+1, karma=F('karma')+5)
+        
+        # Increment the review's rating by one
+        ratings = ['one', 'two', 'three', 'four', 'five']
+        field = ratings[instance.rating-1] + '_star'
+        setattr(instance.item, field, F(field)+1)
+        instance.item.save()
+
