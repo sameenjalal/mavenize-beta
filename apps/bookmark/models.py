650ae694be93616c3b3001a7a656016aad55fbc6
Dennis Ai <dennis.q.ai@gmail.com>
2012-03-31 14:55:32 -0500

    Setup bookmark models

--- apps/bookmark/models.py
+++ apps/bookmark/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+from item.models import Item
+
+class Bookmark(models.Model):
+    user = models.ForeignKey(User)
+    item = models.ForeignKey(Item)
+    is_public = models.BooleanField(default=True)
+    is_done = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return "%s bookmarking Item #%s" % (self.user.get_full_name(),
+            self.item.id)
