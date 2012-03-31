3285e479ce10dfb7a1016efc829cd79788865c5a
Dennis Ai <dennis.q.ai@gmail.com>
2012-03-30 20:03:13 -0500

    Setup item models

---
+++ apps/item/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from item.models import Item
+from item.models import Link
+
+admin.site.register(Item)
+admin.site.register(Link)
--- apps/item/models.py
+++ apps/item/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Item(models.Model):
+    five_star = models.IntegerField(default=0)
+    four_star = models.IntegerField(default=0)
+    three_star = models.IntegerField(default=0)
+    two_star = models.IntegerField(default=0)
+    one_star = models.IntegerField(default=0)
+
+class Link(models.Model):
+    item = models.ForeignKey(Item)
+    partner = models.CharField(max_length=20)
+    url = models.CharField(max_length=200)
+
