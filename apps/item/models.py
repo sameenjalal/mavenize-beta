06c113817d252e5c3061df133b8d7db3ef9b0881
Dennis Ai <dennis.q.ai@gmail.com>
2012-03-30 21:51:14 -0500

    Setup popularity model and signals for item application

--- apps/item/models.py
+++ apps/item/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.core.exceptions import ObjectDoesNotExist
 
 class Item(models.Model):
     five_star = models.IntegerField(default=0)
@@ -11,6 +14,20 @@ class Item(models.Model):
     def __unicode__(self):
         return str(self.id)
 
+    def get_popularity(self):
+        """
+        Returns the Popularity model for this item.
+        """
+        if not hasattr(self, '_popularity_cache'):
+            try:
+                self._popularity_cache = Popularity.objects.get( 
+                    item__id__exact=self.id)
+                self._popularity_cache.item = self
+            except:
+                raise ObjectDoesNotExist 
+        return self._popularity_cache
+
+
 class Link(models.Model):
     item = models.ForeignKey(Item)
     partner = models.CharField(max_length=20)
@@ -18,3 +35,17 @@ class Link(models.Model):
 
     def __unicode__(self):
         return self.url
+
+class Popularity(models.Model):
+    item = models.OneToOneField(Item, primary_key=True)
+    today = models.IntegerField(default=0, db_index=True)
+    week = models.IntegerField(default=0, db_index=True)
+    month = models.IntegerField(default=0, db_index=True)
+    alltime = models.IntegerField(default=0, db_index=True)
+
+    def __unicode__(self):
+        return str(self.alltime)
+
+@receiver(post_save, sender=Item)
+def create_popularity(sender, instance, created, **kwargs):
+    Popularity.objects.create(item=instance)
