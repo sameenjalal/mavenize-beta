e88a90f7b2617181274affc453b794ae6f5ae06b
Dennis Ai <dennis.q.ai@gmail.com>
2012-03-31 20:26:17 -0500

    Setup social graph models

--- apps/social_graph/models.py
+++ apps/social_graph/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Forward(models.Model):
+    source_id = models.BigIntegerField(db_index=True)
+    destination_id = models.BigIntegerField()
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __unicode__(self):
+        return "User #%s following User #%s" % (self.source_id,
+            self.destination_id)
+
+class Backward(models.Model):
+    destination_id = models.BigIntegerField(db_index=True)
+    source_id = models.BigIntegerField()
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __unicode__(self):
+        return "User #%s following User #%s" % (self.source_id,
+            self.destination_id)
