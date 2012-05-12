9ad0e57e0b09c15f233dd8e746d3d22bd0a630d5
Dennis Ai <dennis.q.ai@gmail.com>
2012-05-12 14:27:55 -0500

    Added tests for social graph

--- apps/social_graph/tests.py
+++ apps/social_graph/tests.py
@@ -1,16 +1,41 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
+from notification.models import Notification
+from user_profile.models import KarmaUser
+from social_graph.models import Backward
+import nose.tools as nt
 
-Replace this with more appropriate tests for your application.
-"""
+class TestSocialGraph(object):
+    def setup(self):
+        self.follower = KarmaUser.objects.create(username='a')
+        self.following = KarmaUser.objects.create(username='b')
 
-from django.test import TestCase
-
-
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+    def test_notification(self):
         """
-        Tests that 1 + 1 always equals 2.
+        Tests to see if a notification was created and deleted after
+        a following relationship is created and deleted.
         """
-        self.assertEqual(1 + 1, 2)
+        self.follow = Backward.objects.create(
+            destination_id=self.following.id,
+            source_id=self.follower.id
+        )
+        self.notification = Notification.objects.get(
+            sender=self.follower,
+            recipient=self.following
+        )
+
+        # Tests that a notification was created
+        nt.assert_true(self.notification)
+        nt.assert_equal(self.follow.pk,
+                        self.notification.object_id)
+
+        self.follow.delete()
+        self.notification = Notification.objects.filter(
+            sender=self.follower,
+            recipient=self.following
+        )
+
+        # Tests that the notification was deleted.
+        nt.assert_false(self.notification)
+
+    def teardown(self):
+        self.follower.delete()
+        self.following.delete()
