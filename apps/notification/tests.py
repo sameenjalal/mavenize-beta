8b0703134879e8e5d5fe882ea31157a367d63cfc
Dennis Ai <dennis.q.ai@gmail.com>
2012-05-13 15:29:40 -0500

    Wrote basic tests for redis cache

--- apps/notification/tests.py
+++ apps/notification/tests.py
@@ -1,3 +1,5 @@
+from django.core.cache import get_cache
+
 from user_profile.models import KarmaUser
 from item.models import Item
 from review.models import Review, Agree, Thank
@@ -10,7 +12,7 @@ class TestNotification(object):
     def setup(self):
         self.writer = KarmaUser.objects.create(username='a')
         self.giver = KarmaUser.objects.create(username='b')
-        self.item = Item.objects.create()
+        self.item = Item.objects.create(item_type='pk') # Skip 
         self.review = Review.objects.create(
             user=self.writer, item=self.item, rating=1)
 
@@ -18,7 +20,7 @@ class TestNotification(object):
         self.agree = Agree.objects.create(
             giver=self.giver, review=self.review)
         self.notification = Notification.objects.get(
-            sender_id=self.giver.id)
+            sender=self.giver.id)
 
         # Tests that the recipient is the writer of the review 
         nt.assert_equal(self.notification.recipient_id,
@@ -32,14 +34,14 @@ class TestNotification(object):
         # Tests that the notification has been deleted
         nt.assert_equal(
             list(Notification.objects.filter(
-                sender_id=self.giver.id)),
+                sender=self.giver.id)),
             [])
 
     def test_thanks(self):
         self.thanks = Thank.objects.create(
             giver=self.giver, review=self.review)
         self.notification = Notification.objects.get(
-            sender_id=self.giver.id)
+            sender=self.giver.id)
 
         # Tests that the recipient is the writer of the review
         nt.assert_equal(self.notification.recipient_id,
@@ -53,14 +55,14 @@ class TestNotification(object):
         # Tests that the notification has been deleted
         nt.assert_equal(
             list(Notification.objects.filter(
-                sender_id=self.giver.id)),
+                sender=self.giver.id)),
             [])
 
     def test_following(self):
         self.following = Backward.objects.create(
             destination_id=self.writer.id, source_id=self.giver.id)
         self.notification = Notification.objects.get(
-            sender_id=self.giver.id)
+            sender=self.giver.id)
 
         # Tests that the recipient is the writer of the review
         nt.assert_equal(self.notification.recipient_id,
@@ -74,31 +76,39 @@ class TestNotification(object):
         # Tests that the notification object has been deleted
         nt.assert_equal(
             list(Notification.objects.filter(
-                sender_id=self.giver.id)),
+                sender=self.giver.id)),
             [])
 
-    def test_bookmark(self):
-        self.bookmark = Bookmark.objects.create(
-            user=self.giver, item=self.item)
-        self.notification = Notification.objects.get(
-            sender_id=self.giver.id)
+    def test_redis_notification_queue(self):
+        cache = get_cache('notifications')
+        new_key = "user:" + str(self.writer.id) + ":new"
+        recent_key = "user:" + str(self.writer.id) + ":recent"
+        cache.delete(new_key)
+        cache.delete(recent_key)
 
-        # Tests that the recipient is all-encompassing (0)
-        nt.assert_equal(self.notification.recipient_id, 0)
-        # Tests that the notice object is the bookmark
-        nt.assert_equal(self.notification.notice_object,
-            self.bookmark)
+        self.agree = Agree.objects.create(
+            giver=self.giver, review=self.review)
+        
+        # Tests that the recent notifications length is one
+        nt.assert_equal(cache.get(new_key), 1)
+        nt.assert_equal(len(cache.get(recent_key)), 1)
 
-        self.bookmark.delete()
+        # Tests the characteristics of the notification
+        nt.assert_equal(self.giver.id,
+            cache.get(recent_key)[0]['sender_id'])
+        nt.assert_equal("agree",
+            cache.get(recent_key)[0]['notification_type'])
 
-        # Tests that the notification object has been deleted
-        nt.assert_equal(
-            list(Notification.objects.filter(
-                sender_id=self.giver.id)),
-            [])
+
+        self.thanks = Thank.objects.create(
+            giver=self.giver, review=self.review)
+
+        # Tests that the recent notifications length is two
+        nt.assert_equal(cache.get(new_key), 2)
+        nt.assert_equal(len(cache.get(recent_key)), 2)
+
 
     def teardown(self):
         self.writer.delete()
         self.giver.delete()
         self.item.delete()
-        self.notification.delete()
