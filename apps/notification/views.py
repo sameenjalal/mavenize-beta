6fa971d10f3e026b2e1020beb4d73bbb95f23bea
Dennis Ai <dennis.q.ai@gmail.com>
2012-05-13 23:12:17 -0500

    Added views for notifications

--- apps/notification/views.py
+++ apps/notification/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.contrib.auth.decorators import login_required
 
 import api
@@ -14,9 +14,6 @@ def new_notifications(request):
 
 @login_required
 def recent_notifications(request):
-    if not request.is_ajax():
-        raise Http404
-
     me = request.session['_auth_user_id']
     api.reset_new_notifications_count(me)
     return HttpResponse(api.get_recent_notifications(me),
