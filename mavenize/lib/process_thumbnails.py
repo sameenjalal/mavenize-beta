2f3d398dd0ba4ca44c6decf72cb4f8809a8a25d5
Dennis Ai <dennis.q.ai@gmail.com>
2012-05-30 01:26:02 -0500

    Rewrote thumbnails as a celery task

--- mavenize/lib/process_thumbnails.py
+++ mavenize/lib/process_thumbnails.py
@@ -1,9 +1,20 @@
 from movie.models import Movie
 from sorl.thumbnail import get_thumbnail
+from celery import task
 
-for movie in Movie.objects.all():
-    try:
-        get_thumbnail(movie.image, 'x285')
-    except Exception, e:
-        print "Error processing Movie ID: %s" % (str(movie.pk))
-        print e
+def cache_thumbnails():
+    num_movies = Movie.objects.all().count()
+    per_section = num_movies / 12
+    indexes = range(0, num_movies, per_section)
+    for i in range(len(indexes))[:-1]:
+        cache_subset.delay(indexes[i], indexes[i+1])
+
+
+@task(ignore_result=True)
+def cache_subset(start, end, callback=None)
+    for movie in Movie.objects.all()[start:end]:
+        try:
+            get_thumbnail(movie.image, 'x285')
+        except Exception, e:
+            print "Error processing Movie ID: %s" % (str(movie.pk))
+            print e
