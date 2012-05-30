226ba8d27435e439203fcc95f092881d4cf5ab79
Dennis Ai <dennis.q.ai@gmail.com>
2012-05-30 00:35:22 -0500

    Catching exception for thumbnails

--- mavenize/lib/process_thumbnails.py
+++ mavenize/lib/process_thumbnails.py
@@ -2,4 +2,7 @@ from movie.models import Movie
 from sorl.thumbnail import get_thumbnail
 
 for movie in Movie.objects.all():
-    get_thumbnail(movie.image, 'x285')
+    try:
+        get_thumbnail(movie.image, 'x285')
+    except IOError:
+        print movie.pk
